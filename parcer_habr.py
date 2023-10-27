479d99136bdabd0485b4b069596b654460b3f35a
Gadeonio <95529947+Gadeonio@users.noreply.github.com>
2023-10-27 16:06:54 +0500

    Задача решена, но стоит переписать код в читабельный вид

--- parcer_habr.py
+++ parcer_habr.py
@@ -2,14 +2,14 @@ class IParcerStates:
     def __init__(self, url, tags, fields):
         self.url = url
         self.tags = tags
-        self.dict_sample = dict.fromkeys(fields)
+        self.fields = fields
         self.dicts = []
 
     def parcing(self):
         for i in self.tags:
             self.input_tag()
             states = self.skraping()
-            while (len(state) == len(self.dicts)):
+            while (len(states) == len(self.dicts)):
                 self.dicts.append(self.processing(j))
 
     def input_tag(self):
